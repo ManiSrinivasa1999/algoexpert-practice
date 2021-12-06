5b2e4e1d5bcbc9b16952d193ff21434a73bbdee6
manisrinivasa1999 <manisrinivas1999@gmail.com>
2021-12-06 09:47:03 +0530

    add sorted square array problem E

---
+++ easy/sorted_square_array.py
@@ -0,0 +1,7 @@
+# Write a function that takes in a non-empty array of integers that are sorted
+#   in ascending order and returns a new array of the same length with the squares
+#   of the original integers also sorted in ascending order.
+def sortedSquaredArray(array):
+    result = [int(i)*int(i) for i in array]
+    return sorted(result)
+print(sortedSquaredArray(input().split(' ')))
